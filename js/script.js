3a98555e9da6bfe0309e1cc7fdd5fe72b0444960
SebastianoGuzzon <sebyguzz@icloud.com>
2023-03-21 23:23:56 +0100

    Final commit 2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,51 @@
-const eta = inputEta.value;
-console.log(eta);
+/*
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
-const eta = inputEta.value;
-console.log(eta);
+Steps:
 
-const percorrenza = parseInt(inputKm.value);
-console.log(percorrenza);
+1   Salvo il numero di chilometri da percorrere
+2   Salvo età passeggero 
+3   Verifico se minorenne  
+4   Verifico se over 65
+5   Calcolo Prezzo Totale
+6   Stampo Output su HTML
 
-const prezzoAlKilometro = 0.21;
-const prezzoBigliettoSenzaSconto = prezzoAlKilometro * percorrenza;
-console.log(prezzoBigliettoSenzaSconto);
+ */
 
-const  offerta = "Tariffa Standard"
+const priceKm = 0.21,
+discountUnder18 = 20,
+discountOver65 = 40,
+kmTot = 501, //prompt('Quanti chilometri devi percorrere?'),
+age = 80; //prompt('Quanti anni hai?');
 
-  if (eta == "minorenne") {
-    ((prezzoBigliettoSenzaSconto / 100) * 80);
-    offerta = "Sconto Minorenne";
-} else if (eta == "over65") {
-    ((prezzoBigliettoSenzaSconto / 100) * 60);
-    offerta = "Sconto Maggiorenne";
-} else {
-    (prezzoBigliettoSenzaSconto);
+let message,
+totalPrice;
+
+totalPrice = kmTot * priceKm;
+console.log(totalPrice)
+
+// Calcolo sconti
+
+if(age >= 65){
+  
+  totalPrice *= 1 - (discountOver65/100);
+}else if(age < 18){
+  totalPrice *= 1 - (discountUnder18/100);
 }
+
+message =`
+  Il prezzo del biglietto è di &euro; ${totalPrice.toFixed(2)}.
+ `
+
+document.getElementById('header').innerHTML = message
+
+console.log(priceKm, discountUnder18, discountOver65, kmTot, age)
+console.log('totalPrice',totalPrice)
+console.log('message',message)
+
+document.getElementById('output').innerHTML = message;
